03ebaf83e8aa7ff1d78eb0aa60ef66e352cd0b2e
SANJAY PATTED <sanjaypattedbng@gmail.com>
2025-05-09 12:11:36 +0530

    ice-cream-parlour-example

--- APIS/Notes_Advance/async_await_ex.js
+++ APIS/Notes_Advance/async_await_ex.js
@@ -1,8 +1,17 @@
+//data flow: in a ice cream parlor
+//place order:2 sec
+//select fruit:2 seconds
+//add water and ice:1 sec
+//start machine:1 sec
+//select holder:1sec
+//slect toppings:3 sec
+//serve icecream:2 sec
+
 let rawMaterials={
-    Fruits:[apple,strawberry,banana],
-    Liquid:[ice,water],
-    holder:[stick,cone,cup],
-    toppings:[chocolate,peanuts]
+    Fruits:['apple','strawberry','banana'],
+    Liquid:['ice','water'],
+    holder:['stick','cone','cup'],
+    toppings:['chocolate','peanuts']
 };
 
 let shop_open=true;
@@ -23,11 +32,29 @@ function time(ms){
 async function kitchen() {
     try{
         await time(2000)
-        console.log(`${rawMaterials.Fruits[0]} was selected`);
+        console.log(`${rawMaterials.Fruits[0]} was selected`)
 
+        await time(1000)
+        console.log(`${rawMaterials.Liquid[0]} and  ${rawMaterials.Liquid[1] } were added`)
+
+        await time(1000)
+        console.log(`machine was started`)
+
+        await time(1000)
+        console.log(`${rawMaterials.holder[2]} was selected`)
+
+        await time(3000)
+        console.log(`${rawMaterials.toppings[1]} topping was added`)
+
+        await time(2000)
+        console.log(`Ice cream  was served`)
     }
-    catch(error){}
-    finally{}
+    catch(error){
+        console.log('shop is closed')
+    }
+    finally{
+        console.log('day ended thank you')
+    }
     
 }
 
